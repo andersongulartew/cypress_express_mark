0aa2b106393ce1fcb184a0531e518bff8eb5e80e
andersongulartew <andersongularte1993@gmail.com>
2023-12-07 18:17:03 -0300

    .

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    env: {
+      apiUrl: 'http://localhost:3333'
+    },
+viewportWidth: 1920,
+viewportHeight: 1080,
+
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+            return config;
+    },
+  },
+});
